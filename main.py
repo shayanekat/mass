5758a119857884519bc5e2322c2f7e1203edb40d
Shaykat <shaykatfsx@gmail.com>
2021-01-03 12:28:01 +0300

    added plotting choice

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as pp
+import numpy as np
 
 #=====BACKEND=====
 #init var
 mounth = []
 mass = []
+choice = "annuel"
 
 data = open("data.txt","r")
 for line in data:
@@ -13,16 +15,20 @@ for line in data:
 
 X = []
 Y = []
+if choice == "annuel":
+    for i in range(len(mounth)-12,len(mounth)):
+        X.append(mounth[i])
+        Y.append(mass[i])
 
-for i in range(len(mounth)-12,len(mounth)):
-    X.append(mounth[i])
-    Y.append(mass[i])
+if choice == "tout":
+    for i in range(len(mounth)):
+        X.append(i)
+        Y.append(mass[i])
 
 #=====FRONTEND=====
 pp.grid()
 ax = pp.axes()        
-ax.yaxis.grid() # horizontal lines
-
+ax.yaxis.grid() # vertical lines
 
 pp.plot(X,Y)
 pp.xticks(rotation="vertical",fontsize=7)
