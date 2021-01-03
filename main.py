8a20f8ca45f680f938dc8db2e7f3ca0188cfb108
Shaykat <shaykatfsx@gmail.com>
2021-01-03 13:05:22 +0300

    updated plotting display

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import numpy as np
 #init var
 mounth = []
 mass = []
-choice = "annuel"
+objectif = []
+choice = "tout"
 
 data = open("data.txt","r")
 for line in data:
@@ -19,17 +20,30 @@ if choice == "annuel":
     for i in range(len(mounth)-12,len(mounth)):
         X.append(mounth[i])
         Y.append(mass[i])
+        objectif.append(60)
 
 if choice == "tout":
     for i in range(len(mounth)):
         X.append(i)
         Y.append(mass[i])
+        objectif.append(60)
 
 #=====FRONTEND=====
 pp.grid()
 ax = pp.axes()        
 ax.yaxis.grid() # vertical lines
 
-pp.plot(X,Y)
+# curves
+pp.plot(X, Y, label="Masse")
+pp.plot(X, objectif, "g", label="objectif")
+pp.axhspan(45, 50, alpha=0.5, color='red', label="Minimum limit")
+
+# diplay 
+pp.legend(loc=1)
+pp.xlabel("temps (mois)")
+pp.ylabel("masse (kg)")
+pp.title("Monthly Mass Tracking")
 pp.xticks(rotation="vertical",fontsize=7)
+
+
 pp.show()
